b98e1626987025e91f3f21163e0052389c0ddb6c
Kedar V <kedar54@gmail.com>
2017-01-17 17:17:46 -0500

    write logfile to correct folder

--- task.py
+++ task.py
@@ -11,6 +11,7 @@ from time import sleep
 config = configparser.ConfigParser() 
 scriptDirectory = os.path.dirname(os.path.realpath(__file__))
 settingsFilePath = os.path.join(scriptDirectory, "config.ini")
+logFilePath = os.path.join(scriptDirectory, "log")
 config.readfp(open(settingsFilePath,"r"))
 
 API = config.get('config', 'BASE_API')
@@ -30,7 +31,7 @@ if parsed_json['success'] != True:
   print('exiting...')
   sys.exit(0)
 
-file = open("log", "a+")
+file = open(logFilePath, "a+")
 
 hid = { 4: 'a', 5: 'b', 6: 'c', 7: 'd', 8: 'e', 9: 'f', 10: 'g', 11: 'h', 12: 'i', 13: 'j', 14: 'k', 15: 'l', 16: 'm', 17: 'n', 18: 'o', 19: 'p', 20: 'q', 21: 'r', 22: 's', 23: 't', 24: 'u', 25: 'v', 26: 'w', 27: 'x', 28: 'y', 29: 'z', 30: '1', 31: '2', 32: '3', 33: '4', 34: '5', 35: '6', 36: '7', 37: '8', 38: '9', 39: '0', 44: ' ', 45: '-', 46: '=', 47: '[', 48: ']', 49: '\\', 51: ';' , 52: '\'', 53: '~', 54: ',', 55: '.', 56: '/'  }
 
